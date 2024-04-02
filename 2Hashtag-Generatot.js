bb197e090b4d79c51897ea4266a77a860161dd5c
furajgul <rajeshkumarjena2022@gmail.com>
2024-04-02 23:12:37 +0530

    Day 2

--- 2Hashtag-Generatot.js
+++ 2Hashtag-Generatot.js
@@ -3,7 +3,7 @@
 // You are required to implement a function generateHash that generates a hash taf from a given input string ?
 // The Hash tag should be constructed as follows::
 
-// The input string should be converted in to Hash tag format, where each word is capitalized and concatenated together without spaces.
+// The input string should be converted in to Hash tag format, where each word of 1st character is capitalized and concatenated together without spaces.
 // If the length of the input is greater than 280 characters or if the input is empty or contains only whitespace, the function should return false.
 // Otherwise the Function should return the generated hash tag prefix with #.
 
